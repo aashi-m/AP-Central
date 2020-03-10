105d82a5957b81207e7d03bc78664f03d3b44292
Aashi <aashimittal19@gmail.com>
2020-03-10 14:18:50 -0700

    Added first commonly missed question to the CM Class

---
+++ CommonlyMissed.java
@@ -0,0 +1,19 @@
+
+/**
+ * Required review for commonly missed questions 
+ * @Aashi
+ * @03/10/2020
+ */
+public class CommonlyMissed
+{
+    public static void main(String[] args)
+    {
+        int a = 2/5%3; 
+        int b = 2/(5%3); 
+        int c = 2/5+1; 
+        
+        System.out.println("Expression I evaluates to " + a); 
+        System.out.println("Expression II evaluates to " + b);
+        System.out.println("Expression III evaluates to " + c); 
+    }
+}
--- Sec5B.java
+++ Sec5B.java
@@ -6,27 +6,5 @@
  */
 public class Sec5B
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Sec5B
-     */
-    public Sec5B()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
