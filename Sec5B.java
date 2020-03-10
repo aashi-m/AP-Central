190fbbf87731764e162d589e449218781a745ff2
Aashi <aashimittal19@gmail.com>
2020-03-10 14:04:24 -0700

    Initial sharing of project

---
+++ Sec5B.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Sec5B here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Sec5B
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Sec5B
+     */
+    public Sec5B()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
