e1d462ef4bf6fc951f548571f3d7b241ce393e7b
Aashi <aashimittal19@gmail.com>
2020-03-11 14:17:30 -0700

    asdf

---
+++ Sec6.java
@@ -0,0 +1,57 @@
+
+/**
+ * Missed questions and tests for Section 6
+ * @Aashi
+ * @03/11/2020
+ */
+public class Sec6
+{
+    //  Question 3
+    public static int checkString(String[] arr)
+{   
+    int count = 0;
+    for (int k = 0; k < arr.length; k++)
+    {
+        if (arr[k].length() >= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+public static void main(String[] args)
+{
+    //this one does not compile, because array dimension is missing: checkString(new String[]);
+    checkString(new String[0]);
+    String[] str = {"cat", "dog"};
+    checkString(str);
+    
+    int[] arr = {4, 3, 2, 1, 0};
+int total = 0;
+for (int k = 0; k <= total; k++)
+{
+if (arr[k] % 2 == 0)
+{
+total += arr[k];
+}
+else
+{
+total -= arr[k];
+}
+}
+System.out.println(total);
+
+System.out.print(positionOfFirstNegative(arr));
+}
+
+public static int positionOfFirstNegative(int[] values)
+{
+int index = 0;
+while (values[index] >= 0)
+{
+index++;
+}
+return index;
+}
+}
+
